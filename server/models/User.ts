8ab31afc5f6987b4715b38e4c94bf160e4eda6b4
dinshasivan <dinshasivan@gmail.com>
2026-07-08 11:41:08 +0530

    add eyeIcon for signin/signup page

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    email: {type: String, required: true, unique:true},
-    password: {type: String, required: true, unique:true},
-    name: {type: String, required: true},
-    zernioProfileId: {type:String}
-},{timestamps: true})
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    resetPasswordToken: { type: String },
+    resetPasswordExpires: { type: Date },
+    zernioProfileId: { type: String }
+}, { timestamps: true })
 
 export const User = mongoose.model('User', userSchema)
